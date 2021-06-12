c32d3c0d386eecfd82a1f27f7426d36e08fef5a7
Keru <mikkelsen.aragon@gmail.com>
2021-06-13 00:35:59 +0100

    Add Sms integration

--- watcher_func.cs
+++ watcher_func.cs
@@ -3,6 +3,8 @@ using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Extensions.Logging;
+using Twilio.Rest.Api.V2010.Account;
+using Twilio.Types;
 
 namespace vaccine_watcher
 {
@@ -10,7 +12,8 @@ namespace vaccine_watcher
     {
         [FunctionName("watcher_func")]
         [Timeout("00:00:15")]
-        public static async Task RunAsync([TimerTrigger("0 0/15 * * * *")]TimerInfo myTimer, ILogger log)
+        [return: TwilioSms(AccountSidSetting = "TwilioAccountSid", AuthTokenSetting = "TwilioAuthToken")]
+        public static async Task<CreateMessageOptions> RunAsync([TimerTrigger("0 0/15 * * * *")]TimerInfo myTimer, ILogger log)
         {
             log.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
             log.LogInformation($"Next schedule: {myTimer.ScheduleStatus.Next}");
@@ -22,19 +25,41 @@ namespace vaccine_watcher
                 if (!response.IsSuccessStatusCode)
                 {
                     log.LogError($"Status code is not success => code: {response.StatusCode}");
-                    return;
+                    return null;
                 }
 
                 var siteContent = await response.Content.ReadAsStringAsync();
 
+                var msg = string.Empty;
                 if (siteContent.Contains("30 to 69", StringComparison.OrdinalIgnoreCase))
                 {
-                    log.LogInformation("Vaccine is now available for 30s and above!");
+                    msg = "Vaccine is now available for 30s and above!";
+                    
+                    log.LogInformation(msg);
+
+                    try
+                    {
+                        var phoneNumberFrom = Environment.GetEnvironmentVariable("TwilioFromPhoneNumber", EnvironmentVariableTarget.Process);
+                        var phoneNumberTo = Environment.GetEnvironmentVariable("TwilioToPhoneNumber", EnvironmentVariableTarget.Process);
+                        var message = new CreateMessageOptions(new PhoneNumber(phoneNumberTo))
+                        {
+                            From = new PhoneNumber(phoneNumberFrom),
+                            Body = msg
+                        };
+
+                        return message;
+                    }
+                    catch (Exception ex)
+                    {
+                        log.LogError(ex, "Exception occurred when sending sms.");
+                    }
                 }
                 else
                 {
-                    log.LogInformation("Vaccine is not yet available for 30s.");
+                    msg = "Vaccine is not yet available for 30s.";
+                    log.LogInformation(msg);
                 }
+                return null;
             }
         }
     }
